f6a9b18cc5bc7dfd10c85bd993418009542495c2
alsami6 <alsami1209@gmail.com>
2024-09-15 14:26:52 +0600

    _

---
+++ main.js
@@ -0,0 +1,10 @@
+const name = "sami";
+const repoCount = "45"
+
+
+console.log(`Hello my name in ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String("samiop")
+
+console.log(gameName[0]);
