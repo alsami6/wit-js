e20fb9ca2d6da95a97ba89ad94687895ba36af92
alsami6 <alsami1209@gmail.com>
2024-09-23 09:35:38 +0600

    Array

---
+++ problemArray.js
@@ -0,0 +1,61 @@
+//  q:!
+
+let array1 = ['apple', 'banana', 'cherry']
+console.log(array1.toString());
+
+// q:2
+
+let array2 = [10, 20, 30, 40, 50]
+console.log(array2.at(2));
+
+// q:3
+
+let array3 = ['red', 'green', 'blue']
+console.log(array3.join("-"));
+
+// q:4
+
+let array4 = [1, 2, 3, 4, 5];
+let newArray4 = array4.pop();
+console.log(array4);
+
+// q:5
+
+let array5 = ['apple', 'banana']
+array5.push('grape')
+console.log(array5);
+
+// q:6
+
+let array6 = ['car', 'bike', 'bus']
+array6.shift();
+console.log(array6);
+
+// q:7
+
+let array7 = ['boat', 'ship']
+array7.unshift('train', 'plane')
+console.log(array7);
+
+// q:8
+
+let array8 = ['dog', 'cat', 'bird']
+array8.splice(1,1)
+console.log(array8);
+
+// q:9
+
+let arr1 = [1,2,3]
+let arr2 = [4,5,6]
+
+let arr3 = [...arr1, ...arr2]
+console.log(arr3);
+
+// q:10
+
+let array10 = ["a", "b", "c", "d", "e", "f"]
+array10.splice(2,2)
+console.log(array10);
+
+
+// q:11
